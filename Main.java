fccc2bdc9cc0ba6ffeac7813c66118db47fc4d68
gulinkonstantin <exaggerateconsciousness@gmail.com>
2026-04-09 15:56:56 +0300

    Change in main for merge

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 class Main {
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main");
-
-        System.out.println("Изменение 1");
-
-        System.out.println("Изменение 2");
-
-        System.out.println("Изменение 3");
-
-        System.out.println("Изменение 4");
+        System.out.print("Изменение main перед слиянием");
     }
 }
