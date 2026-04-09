449e1b20910a38f5e9c50ab06f0af66055f980e2
gulinkonstantin <exaggerateconsciousness@gmail.com>
2026-04-09 15:46:48 +0300

    Изменение 1 для ветки dev

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке dev");
+
+        System.out.println("Изменение 1 для ветки dev");
     }
 }
